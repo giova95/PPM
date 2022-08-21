9f3aae1e791bc3acd47cb9ae600f2d59e7357f1d
giova <giovanni.angeli1@stud.unifi.it>
2022-08-22 00:38:32 +0200

    add red x, title author desc in full, try to limit

--- scripts/speechAPI.js
+++ scripts/speechAPI.js
@@ -118,27 +118,39 @@ request.onload = function () {
 
                item1.addEventListener("click", item1Click);
                 function item1Click() {
-                    $('#full').attr('src', 'gallery/canipoker.jpg');
+                    document.getElementById("full-title").innerHTML = pictures[0].title;
+                    document.getElementById("author").innerHTML = 'Di ' + pictures[0].author + ' completato nel ' + pictures[0].date;
+                    document.getElementById("full-description").innerHTML = pictures[0].description;                    
+                    $('#full').attr('src', 'gallery/primavera-di-botticelli.jpg');
                     $('#image-full').fadeIn("slow");// decidere se fadeIn, show o slideDown
                     $('#item-container').fadeTo("slow", 0.2);
                     $('.navbar').fadeTo("slow", 0.2);
                 }
                 item2.addEventListener("click", item2Click);
                 function item2Click() {
-                    $('#full').attr('src', 'gallery/fucilazione.jpg');
+                    document.getElementById("full-title").innerHTML = pictures[1].title;
+                    document.getElementById("author").innerHTML = 'Di ' + pictures[1].author + ' completato nel ' + pictures[1].date;
+                    document.getElementById("full-description").innerHTML = pictures[1].description;
+                    $('#full').attr('src', 'gallery/persistenza.jpg');
                     $('#image-full').fadeIn("slow");// decidere se fadeIn, show o slideDown
                     $('#item-container').fadeTo("slow", 0.2);
                     $('.navbar').fadeTo("slow", 0.2);
                 }
                 item3.addEventListener("click", item3Click);
                 function item3Click() {
-                    $('#full').attr('src', 'gallery/isolagrande.jpg');
+                    document.getElementById("full-title").innerHTML = pictures[2].title;
+                    document.getElementById("author").innerHTML = 'Di ' + pictures[2].author + ' completato nel ' + pictures[2].date;
+                    document.getElementById("full-description").innerHTML = pictures[2].description;
+                    $('#full').attr('src', 'gallery/tsunami.jpg');
                     $('#image-full').fadeIn("slow");// decidere se fadeIn, show o slideDown
                     $('#item-container').fadeTo("slow", 0.2);
                     $('.navbar').fadeTo("slow", 0.2);
                 }
                 item4.addEventListener("click", item4Click);
                 function item4Click() {
+                    document.getElementById("full-title").innerHTML = pictures[3].title;
+                    document.getElementById("author").innerHTML = 'Di ' + pictures[3].author + ' completato nel ' + pictures[3].date;
+                    document.getElementById("full-description").innerHTML = pictures[3].description;
                     $('#full').attr('src', 'gallery/liberta.png');
                     $('#image-full').fadeIn("slow");// decidere se fadeIn, show o slideDown
                     $('#item-container').fadeTo("slow", 0.2);
@@ -146,38 +158,63 @@ request.onload = function () {
                 }
                 item5.addEventListener("click", item5Click);
                 function item5Click() {
-                    $('#full').attr('src', 'gallery/frutta.jpg');
+                    document.getElementById("full-title").innerHTML = pictures[4].title;
+                    document.getElementById("author").innerHTML = 'Di ' + pictures[4].author + ' completato nel ' + pictures[4].date;
+                    document.getElementById("full-description").innerHTML = pictures[4].description;
+                    $('#full').attr('src', 'gallery/fucilazione.jpg');
                     $('#image-full').fadeIn("slow");// decidere se fadeIn, show o slideDown
                     $('#item-container').fadeTo("slow", 0.2);
                     $('.navbar').fadeTo("slow", 0.2);
                 }
                 item6.addEventListener("click", item6Click);
                 function item6Click() {
-                    $('#full').attr('src', 'gallery/persistenza.jpg');
+                    document.getElementById("full-title").innerHTML = pictures[5].title;
+                    document.getElementById("author").innerHTML = 'Di ' + pictures[5].author + ' completato nel ' + pictures[5].date;
+                    document.getElementById("full-description").innerHTML = pictures[5].description;
+                    $('#full').attr('src', 'gallery/canipoker.jpg');
                     $('#image-full').fadeIn("slow");// decidere se fadeIn, show o slideDown
                     $('#item-container').fadeTo("slow", 0.2);
                     $('.navbar').fadeTo("slow", 0.2);
                 }
                 item7.addEventListener("click", item7Click);
                 function item7Click() {
-                    $('#full').attr('src', 'gallery/primavera-di-botticelli.jpg');
+                    document.getElementById("full-title").innerHTML = pictures[6].title;
+                    document.getElementById("author").innerHTML = 'Di ' + pictures[6].author + ' completato nel ' + pictures[6].date;
+                    document.getElementById("full-description").innerHTML = pictures[6].description;                
+                    $('#full').attr('src', 'gallery/isolagrande.jpg');
                     $('#image-full').fadeIn("slow");// decidere se fadeIn, show o slideDown
                     $('#item-container').fadeTo("slow", 0.2);
                     $('.navbar').fadeTo("slow", 0.2);
                 }
                 item8.addEventListener("click", item8Click);
                 function item8Click() {
-                    $('#full').attr('src', 'gallery/tsunami.jpg');
+                    document.getElementById("full-title").innerHTML = pictures[7].title;
+                    document.getElementById("author").innerHTML = 'Di ' + pictures[7].author + ' completato nel ' + pictures[7].date;
+                    document.getElementById("full-description").innerHTML = pictures[7].description;
+                    $('#full').attr('src', 'gallery/frutta.jpg');
                     $('#image-full').fadeIn("slow");// decidere se fadeIn, show o slideDown
                     $('#item-container').fadeTo("slow", 0.2);
                     $('.navbar').fadeTo("slow", 0.2);
                 }
                 item9.addEventListener("click", item9Click);
                 function item9Click() {
+                    document.getElementById("full-title").innerHTML = pictures[8].title;
+                    document.getElementById("author").innerHTML = 'Di ' + pictures[8].author + ' completato nel ' + pictures[8].date;
+                    document.getElementById("full-description").innerHTML = pictures[8].description;
                     $('#full').attr('src', 'gallery/viandante.jpg');
                     $('#image-full').fadeIn("slow");// decidere se fadeIn, show o slideDown
                     $('#item-container').fadeTo("slow", 0.2);
                     $('.navbar').fadeTo("slow", 0.2);
+                }
+
+                escfull.addEventListener("mouseover", overXred);
+                function overXred(){
+                    $("#x-full").attr('src', 'img/x-red.png');
+                }
+
+                escfull.addEventListener("mouseout", outXnormal);
+                function outXnormal(){
+                    $("#x-full").attr('src', 'img/icons8-x-96.png');
                 }
 
                 escfull.addEventListener("click", escfullClick);
@@ -189,13 +226,12 @@ request.onload = function () {
 
                 var N = pictures.length;
                 
-                
                 for(var i = 1; i<=N; i++) {
                     document.getElementById("image-title"+i).innerHTML = pictures[i-1].title;
+                    /*let desc = pictures[i-1].description;
+                    let limit = desc.substring(0, 190);*/
                     document.getElementById("image-tag"+i).innerHTML = pictures[i-1].description;
                 }
-
-                
             }
             else {
                 console.log("Il tuo Browser non supporta il riconoscimento vocale")
