957a7a39415521b9f07943ab9d8969f83db1ac4d
Beccio00 <riccardo.becciolini@stud.unifi.it>
2022-08-27 12:42:26 +0200

    prova coco-ssd

--- scripts/AI.js
+++ scripts/AI.js
@@ -1,30 +1,16 @@
-/*let img;
-let detector;
-
-function preload() {
-    img = loadImage('gallery/canipoker.jpg');
-    detector = m15.objectDetector('cocossd');
-    
-}
-
-function setup() {
-    createCanvas(640, 480);
-    console.log(detector);
-    Image(imgg, 0, 0)
-}
-
-*/
 
     // Notice there is no 'import' statement. 'cocoSsd' and 'tf' is
     // available on the index-page because of the script tag above.
 
-    const img = document.getElementById('image-main5');
+    const img = document.getElementById('image-main7');
 
     // Load the model.
     cocoSsd.load().then(model => {
         // detect objects in the image.
         model.detect(img).then(predictions => {
-            console.log('Predictions: ', predictions);
+            console.log(predictions.length);
+            for(let i = 1; i<=predictions.lenght; i++){
+                console.log(predictions[0].class);
+            }
         });
     });
-
