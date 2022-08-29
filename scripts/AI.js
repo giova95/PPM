30cb34f64ca20bcc17fd44756d6f2ef6d538162e
Beccio00 <riccardo.becciolini@stud.unifi.it>
2022-08-29 13:28:57 +0200

    Tag-descrizione

--- scripts/AI.js
+++ scripts/AI.js
@@ -1,27 +1,47 @@
 
-const pictures1 = req.response;
+const pictures = req.response;
 
-let n = pictures1.length;
+let n = pictures.length;
 
 window.onload = function () {
 
     for (let i = 1; i <= n; i++) {
         let img = document.getElementById('image-main' + i);
 
-    // Load the model.
-    cocoSsd.load().then(model => {
-        // detect objects in the image.
-        model.detect(img).then(predictions => {
-            let k = predictions.length;
+        // Load the model.
+        cocoSsd.load().then(model => {
+            // detect objects in the image.
+            model.detect(img).then(predictions => {
+                let k = predictions.length;
+                for (s = 0; s < k; s++) {
+                    console.log(predictions[s].class);
+                    if (pictures[i - 1].tags)
+                        pictures[i - 1].tags = pictures[i - 1].tags + " " + predictions[s].class;
+                    else
+                        pictures[i - 1].tags = predictions[s].class;
+
+                }
+            });
             
             
-            for (s = 0; s < k; s++) {
-                console.log(predictions[s].class);
-                pictures1[i-1].tags= predictions[s].class;
+
+            for (let i = 1; i <= n; i++) {
+                item = document.querySelector("#item" + i);
+                if (item) {
+                    item.addEventListener("click", itemClick);
+                    function itemClick() {
+                        tags = document.getElementById("tags");
+                        if(tags){
+                            if(pictures[i - 1].tags)
+                                tags.innerHTML ="Tag: " + pictures[i-1].tags;
+                            else
+                                tags.innerHTML ="Tag: ";
+                        }
+                    }
+                }
             }
         });
-    });
-}
+    }
 
     console.log(pictures);
 };
