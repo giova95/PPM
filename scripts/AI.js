5f0a497b79ae445ac7df2663d5fbc8daf131fd34
Beccio00 <riccardo.becciolini@stud.unifi.it>
2022-12-14 20:51:06 +0100

    Riaggiustamneto AI

--- scripts/AI.js
+++ scripts/AI.js
@@ -7,36 +7,45 @@ window.onload = function () {
 
     for (let i = 1; i <= n; i++) {
         let img = document.getElementById('image-main' + i);
+        let l = picture.tags[i - 1];
 
         // Load the model.
-        cocoSsd.load().then(model => {
-            // detect objects in the image.
-            model.detect(img).then(predictions => {
-                let k = predictions.length;
-                for (let s = 0; s < k; s++) {
-                    console.log(predictions[s].class);
-                    if (picture[i - 1].tags)
-                        picture[i - 1].tags = picture[i - 1].tags + " " + predictions[s].class;
-                    else
-                        picture[i - 1].tags = predictions[s].class;
-
-                }
-            });
-
-            item = document.querySelector("#item" + i);
-            if (item) {
-                item.addEventListener("click", itemClick);
-                function itemClick() {
-                    tags = document.getElementById("tags");
-                    if (tags) {
-                        if (picture[i - 1].tags)
-                            tags.innerHTML = "Tag: " + picture[i - 1].tags;
+        if (!l) {
+            cocoSsd.load().then(model => {
+                // detect objects in the image.
+                model.detect(img).then(predictions => {
+                    let k = predictions.length;
+                    for (let s = 0; s < k; s++) {
+                        console.log(predictions[s].class);
+                        /*if (picture[i - 1].tags)
+                            picture[i - 1].tags = picture[i - 1].tags + " " + predictions[s].class;
                         else
-                            tags.innerHTML = "Tag: ";
+                            picture[i - 1].tags = predictions[s].class;
+                        */
+
+                        if (l)
+                            l = l + " " + predictions[s].class;
+                        else
+                            l = predictions[s].class;
+                    }
+                    picture[i - 1].tags = l;
+                });
+
+                item = document.querySelector("#item" + i);
+                if (item) {
+                    item.addEventListener("click", itemClick);
+                    function itemClick() {
+                        tags = document.getElementById("tags");
+                        if (tags) {
+                            if (picture[i - 1].tags)
+                                tags.innerHTML = "Tag: " + picture[i - 1].tags;
+                            else
+                                tags.innerHTML = "Tag: ";
+                        }
                     }
                 }
-            }
-        });
+            });
+        }
     }
 
     console.log(picture);
