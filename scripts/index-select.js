11d8e71097c37fb90ccf6daa146f448c999e07fa
Beccio00 <riccardo.becciolini@stud.unifi.it>
2022-12-21 13:12:24 +0100

    update

--- scripts/index-select.js
+++ scripts/index-select.js
@@ -1,6 +1,6 @@
-let urlApi = 'http://localhost:3307';
+let urlApi2 = 'http://localhost:3307';
 let req = new XMLHttpRequest();
-req.open('GET', urlApi);
+req.open('GET', urlApi2);
 req.responseType = 'json';
 req.send();
 
@@ -30,6 +30,9 @@ req.onload = function () {
             const escfull = document.querySelector("#x-full");
             const maincontainer = document.querySelector(".main-container");
             const itemcontainer = document.querySelector("#item-container");
+            var nImg;
+
+            if(itemcontainer){
 
 
                 for (let i = 1; i <= N; i++) {
@@ -85,180 +88,22 @@ req.onload = function () {
                     pen2.id = "pencil" + i;
                     pen2.src = "img/Pencil.png";
                     pendel2.appendChild(pen2);
-                }
 
-
-
-
-
-                micBtn.addEventListener("click", micBtnClick);
-                function micBtnClick() {
-                    if (!rec) {
-                        recognition.start();
-                    }
-                    else {
-                        recognition.stop();
-                    }
-                    console.log(rec);
-                }
-
-                micBtn.addEventListener("mouseover", micBtnOver);
-                function micBtnOver() {
-                    $('.mic1').attr('src', 'img/mic-gray.png');
-                }
-
-                micBtn.addEventListener("mouseout", micBtnOut);
-                function micBtnOut() {
-                    if (!rec) {
-                        $('.mic1').attr('src', 'img/mic.png');
-                    } else {
-                        $('.mic1').attr('src', 'img/mic-red.png');
-                    }
-                }
-
-                search.addEventListener("click", searchClick);
-                function searchClick() {
-                    $('.menu').fadeTo("fast", 0);
-
-                    $('.vocal-search').fadeTo("fast", 0);
-
-                    $('#search-box').fadeIn('fast');
-
-                    $('.suggestion').fadeIn('fast');
-
-                    $("#navbar").css({ 'border': 'none', 'background': 'transparent' });
-                }
-
-                search.addEventListener("mouseover", searchselect);
-                function searchselect() {
-                    $('#search').css('color', '#949494');
-                    $('#mic2').attr('src', 'img/mic-gray.png');
-                }
-
-                search.addEventListener("mouseout", searchunselect);
-                function searchunselect() {
-                    $('#search').css('color', '#000000');
-                    $('#mic2').attr('src', 'img/mic.png');
-                }
-
-                esc.addEventListener("click", escClick);
-                function escClick() {
-                    $('.menu').fadeTo("fast", 1);
-
-                    $('.vocal-search').fadeTo("fast", 1);
-
-                    $('#search-box').fadeOut('fast');
-                    $('.suggestion').fadeOut('fast');
-
-                    $("#navbar").css({ 'border-bottom': '1px solid lightgray', 'background': 'white' });
-                    
-                    $('.search-result').css({
-                        'opacity': '0'
+                    a2.addEventListener("click", function(){
+                        nImg = i - 1;
+                        return nImg;
                     });
                     
-                    for (let i = 1; i <= N; i++) {
-                        $('#item' + i).css('display', 'block');
-                    }
-                }
-
-                esc.addEventListener("mouseover", escselect);
-                function escselect() {
-                    $('#x-icon').attr('src', 'img/x-gray.png');
-                }
-                esc.addEventListener("mouseout", escunselect);
-                function escunselect() {
-                    $('#x-icon').attr('src', 'img/x.png');
-                }
-
-                recognition.addEventListener("start", startSpeechRecognition);
-                function startSpeechRecognition() {
-                    $('.mic1').attr('src', 'img/mic-red.png');
-                    $('#mic1-mobile').attr('src', 'img/mic-red-mobile.png');
-                    $('.search-result').css({
-                        'opacity': '0'
-                    });
-                    rec = true;
-                    //formInput.focus();
-                    console.log("Vocal search on");
-                    for (let i = 1; i <= N; i++) {
-                        $('#item' + i).css('display', 'block');
-                    }
-                }
-
-                recognition.addEventListener("end", endSpeechRecognition);
-                function endSpeechRecognition() {
-                    $('.mic1').attr('src', 'img/mic.png');
-                    $('#mic1-mobile').attr('src', 'img/mic-mobile.png');
-                    $('.suggestion').fadeOut("slow");
-                    $('.search-result').css({
-                        'opacity': '1'
-                    })
-                    rec = false;
-                    console.log("Vocal search off");
-                    let remove = 0;
-                    var tagsrc = document.searchForm.q.value.toLowerCase();
-                    for (let i = 1; i <= N; i++) {
-                        var tagsearch = picture[i - 1].tags.search(tagsrc);
-                        if (tagsearch == "-1") {
-                            console.log("remove image" + picture[i - 1].id);
-                            $('#item' + i).css('display', 'none');
-                            remove++;
-                        }
-                    }       
-                }
-
-                recognition.addEventListener("result", resultSpeechRecognition);
-                function resultSpeechRecognition(event) {
-                    const transcript = event.results[0][0].transcript;
-                    formInput.value = transcript;
-                };
-
-
-                const mediaQuery = window.matchMedia('(min-width: 1000px)');
-
-
-
-
-                mediaQuery.addListener(media);
-
-                media(mediaQuery);
-
-                if (escfull) {
-
-                    escfull.addEventListener("mouseover", overXred);
-                    function overXred() {
-                        $("#x-full").attr('src', 'img/x-red.png');
-                    }
-
-                    escfull.addEventListener("mouseout", outXnormal);
-                    function outXnormal() {
-                        $("#x-full").attr('src', 'img/icons8-x-96.png');
-                    }
-
-                    escfull.addEventListener("click", escfullClick);
-                    function escfullClick() {
-                        $('#image-full').slideUp();
-                        $('#item-container').fadeTo("slow", 1);
-                        $('.navbar').fadeTo("slow", 1);
-                    }
-                }
-
-                for (let i = 1; i <= N; i++) {
-                    title = document.getElementById("image-title" + i);
-                    author = document.getElementById("image-author" + i);
-                    date = document.getElementById("image-date" + i);
-                    tag = document.getElementById("image-tag" + i)
                     
-                    
-                    if (title && author && date && tag) {
-                        title.innerHTML = pictures[i - 1].title;
-                        author.innerHTML = pictures[i - 1].author;
-                        date.innerHTML = pictures[i - 1].date;
-                        tag.innerHTML = "Tag: " + pictures[i - 1].tags;
-                        console.log(pictures[i -1].id);
-                    }
                 }
 
+                
+            }
+
+            console.log(nImg);
+            console.log(pictures[nImg]);
+                
+                
         }
     )
 };
